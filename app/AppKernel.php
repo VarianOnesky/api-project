0fda6b0cecd48d2028b13c4cff3c7e79ec9939a9
VarianOnesky <varianng@NgVarians-MacBook-Air-2.local>
2015-03-12 10:55:18 +0800

    Add the bundle to AppKernel

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -20,6 +20,7 @@ class AppKernel extends Kernel
             new FOS\RestBundle\FOSRestBundle(),
             new JMS\SerializerBundle\JMSSerializerBundle(),
             new Acme\ApiBundle\AcmeApiBundle(),
+            new FOS\OAuthServerBundle\FOSOAuthServerBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
